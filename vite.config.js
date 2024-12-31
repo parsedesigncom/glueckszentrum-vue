0dfd443d5cd1e1c1332a6483c55aa1c21a2f64bc
saeid.samani <saeid.samani@nsgroup.ch>
2024-12-31 13:30:24 +0100

    setup vite config for build ans some mor alias

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,37 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src'),
+      '@font': path.resolve(__dirname, './src/fonts/'),
+      '@img': path.resolve(__dirname, './src/assets/images/')
     }
   },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          // Prüfen, ob die Datei ein Font ist
+          const fontExtensions = ['.woff', '.woff2', '.ttf', '.otf', '.eot'];
+          const svgExtensions = ['.svg'];
+          const imgExtensions = ['.gif', '.png', '.jpeg'];
+
+          if (fontExtensions.some(ext => assetInfo.name.endsWith(ext))) {
+            return 'assets/fonts/[name][extname]';
+          }
+          if (imgExtensions.some(ext => assetInfo.name.endsWith(ext))) {
+            return 'assets/img/[name][extname]';
+          }
+          if (svgExtensions.some(ext => assetInfo.name.endsWith(ext))) {
+            return 'assets/svg/[name][extname]';
+          }
+
+          // Standardmäßig in assets-Ordner ablegen
+          return 'assets/[name][extname]';
+        },
+        // Optionale zusätzliche Struktur für JS und CSS
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+      },
+    },
+  }
 })
