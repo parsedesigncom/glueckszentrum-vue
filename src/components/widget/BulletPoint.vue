1d24c371d0663fc7455cd0d66653d402302ce4e0
saeid.samani <saeid.samani@nsgroup.ch>
2025-01-01 22:30:28 +0100

    add Bullet point Component

---
+++ src/components/widget/BulletPoint.vue
@@ -0,0 +1,115 @@
+<script>
+export default {
+  name: "BulletPoint",
+  props: {
+    items: {
+      type: Array,
+      required: true,
+      default: () => []
+    },
+    cssClass: {
+      type: String,
+      required: false,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      svgIcons: {},
+      missingIcon: `x`
+    };
+  },
+  created() {
+    this.loadSvgIcons();
+  },
+  methods: {
+    loadSvgIcons() {
+      const svgModules = import.meta.glob('@/assets/images/*.svg', {as: 'raw'});
+
+      // Lädt alle SVGs und speichert sie in `svgIcons`
+      for (const [path, loader] of Object.entries(svgModules)) {
+        const fileName = path.split('/').pop().replace('.svg', ''); // Extrahiert den Dateinamen
+        loader().then((content) => {
+          this.svgIcons[fileName] = content;
+        });
+      }
+    }
+  }
+};
+</script>
+
+<template>
+  <div v-if="items && items.length" :class="['component-holder-BulletPoint', cssClass]">
+    <ul class="bullet-point-items">
+      <li v-for="(item, index) in items" :key="index" class="bullet-point-item">
+        <div class="icon" v-html="svgIcons[item.icon] || missingIcon"></div>
+        <div class="content" v-html="item.content"></div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.component-holder-BulletPoint {
+  ul.bullet-point-items {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    text-align: left;
+
+    li.bullet-point-item {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      gap: 10px;
+
+      div {
+        &.icon {
+          display: inline-flex;
+          align-items: center;
+          justify-content: center;
+          width: 25px;
+          height: 25px;
+          flex-shrink: 0;
+
+          svg {
+            width: 100%;
+            height: 100%;
+          }
+        }
+
+        &.content {
+          flex: 1;
+        }
+      }
+    }
+  }
+  &.use-in-homepage{
+
+    ul.bullet-point-items {
+      li.bullet-point-item {
+        max-width: 600px;
+        margin-bottom: 15px;
+        div {
+          &.icon {
+            fill: $color-accent;
+            width: 38px;
+            height: 38px;
+            align-self: start;
+          }
+          &.content {
+            font-family: $font-family-montserrat;
+            font-weight: 400;
+            @include responsive-text($font-accent, $font-accent);
+            ::v-deep(strong) {
+              font-weight: 600;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
+</style>
