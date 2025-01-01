fb53bf1cc4cfea96c2ed96d8b6fdd37196ca3aa2
saeid.samani <saeid.samani@nsgroup.ch>
2025-01-01 23:25:03 +0100

    add Area Component

---
+++ src/components/widget/AreaComponent.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  name: "AreaComponent",
+  props: {
+    bgImgSrc: {
+      type: String,
+      required: false,
+      default: '@assets/images/bg-transparent.png'
+    },
+    cssClass: {
+      type: String,
+      required: false,
+      default: ""
+    }
+  },
+}
+</script>
+
+<template>
+  <div :class="['component-holder-AreaComponent', cssClass]" :style="`--background-image: url(${bgImgSrc});`">
+    <slot></slot>
+  </div>
+
+</template>
+
+<style scoped lang="scss">
+
+.component-holder-AreaComponent{
+  box-sizing: border-box;
+  padding: 4rem 1rem;
+  background-color: $color-bg-gray ;
+  background-image: var(--background-image);
+  background-size: auto;
+  background-repeat: repeat;
+  background-position: left top;
+  &.bg-color-white{
+    background-color: $color-white;
+  }
+}
+
+</style>
